4a1025250bc041fb90561c056900130cb319ed3f
Lavkant Kachhwaha <lavkant.kachhwaha@lithionpower.com>
2019-09-25 11:33:40 +0530

    ideal reward function from tutorial

--- reward_functions/centerLine.py
+++ reward_functions/centerLine.py
@@ -1,46 +1,49 @@
 def reward_function(params):
-  max_reward = 1
-  min_reward = 0.1
-  reward = 0
+    max_reward = 10
+    min_reward = 1
+    reward = 0
 
-  max_speed = 5
-  min_speed= 0.25
+    max_speed = 5
+    min_speed = 0.25
 
-  marker_1 = 0.1 * params['track_width']
-  marker_2 = 0.2 * params['track_width']
-  marker_3 = 0.3 * params['track_width']
-  marker_4 = 0.4 * params['track_width']
-  marker_5 = 0.5 * params['track_width']
+    marker_1 = 0.1 * params['track_width']
+    marker_2 = 0.2 * params['track_width']
+    marker_3 = 0.3 * params['track_width']
+    marker_4 = 0.4 * params['track_width']
+    marker_5 = 0.5 * params['track_width']
 
-  if params['distance_from_center'] <= marker_1:
-    reward += 1
-  elif params['distance_from_center'] <= marker_2:
-    reward += (max_reward-(2*min_reward))
-  elif params['distance_from_center'] <= marker_3:
-    reward += (max_reward-(4*min_reward))
-  elif params['distance_from_center'] <= marker_4:
-    reward += (max_reward-(6*min_reward))
-  elif params['distance_from_center'] <= marker_5:
-    reward += (max_reward-(9*min_reward))
-  else:
-    reward = 1e-3  
-  
+    if params['distance_from_center'] <= marker_1:
+        reward += max_reward
+    elif params['distance_from_center'] <= marker_2:
+        reward += (max_reward-(1*min_reward)) #9 
+    elif params['distance_from_center'] <= marker_3:
+        reward += (max_reward-(5*min_reward)) #5
+    elif params['distance_from_center'] <= marker_4:
+        reward += (max_reward-(9*min_reward)) #1
+    elif params['distance_from_center'] <= marker_5:
+        reward = 1e-3                   #0.003
 
-  if (params['all_wheels_on_track']):
-      reward += max_reward
-  else:
-      reward -= max_reward
+    if (params['all_wheels_on_track']):
+        reward *= max_reward
+    else:
+        reward /= max_reward
 
 # maximum punishment if not in route
-  if(params['is_reversed']):
-    reward -= max_reward
+    if(params['is_reversed']):
+        reward = 1e-3
+
+    # checking its in corner and speed is maximum so it should be maximum minus reward
+    if params['distance_from_center'] <= marker_5:
+        if abs(params['speed']) > 0.8:
+            reward = 1e-3
+    
+    # if he is in center line then speed max
+    if params['distance_from_center'] <= marker_1 or params['distance_from_center'] <= marker_2 :
+        if abs(params['speed']) > 3 :
+            reward *= max_reward
+    
 
 
-  # checking its in corner and speed is maximum so it should be maximum minus reward
-  if params['distance_from_center'] <= marker_5 :
-    if abs(params['speed']) > 1:
-      reward -= max_reward
-  
 
 
-  return float(reward)
+    return float(reward)
